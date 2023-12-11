12a9c47ad93e627f63d83c1fdd4036f345abb1b3
Jean <350750477@gapps.yrdsb.ca>
2023-12-11 13:09:30 -0500

    Upload start-off files

--- GameWorld2.java
+++ GameWorld2.java
@@ -1,21 +1,21 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * Write a description of class GameWorld2 here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class MyWorld extends World
+public class GameWorld2 extends World
 {
 
     /**
-     * Constructor for objects of class MyWorld.
+     * Constructor for objects of class GameWorld2.
      * 
      */
-    public MyWorld()
+    public GameWorld2()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1);
+        super(600, 400, 1); 
     }
 }
