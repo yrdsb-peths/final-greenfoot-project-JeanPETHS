dc66aa1e7f3bed3ba0366642cc2e3e674d65448e
Jean <350750477@gapps.yrdsb.ca>
2023-12-20 12:34:48 -0500

    Add box features & change tile map

--- Box.java
+++ Box.java
@@ -8,13 +8,24 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Box extends Tile
 {
+    GameWorld1 gameWorld1;
+    
     public Box()
     {
         super(new GreenfootImage("locked-box.png"));
     }
     
+    public void addedToWorld(World world)
+    {
+        if(world instanceof GameWorld1)
+        {
+            gameWorld1 = (GameWorld1)world;
+        }
+    }
+    
     public void unlock()
     {
         super.setTileImage(new GreenfootImage("unlocked-box.png"));
+        gameWorld1.addObject(new Tile(37), 9*24+12, 400-9*24-12);
     }
 }
