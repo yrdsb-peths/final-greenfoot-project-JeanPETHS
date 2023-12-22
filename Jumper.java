7a4006a9008efbe2486e65a708892be883973f24
Jean <350750477@gapps.yrdsb.ca>
2023-12-21 21:09:41 -0500

    Check if above jumper

--- Jumper.java
+++ Jumper.java
@@ -8,6 +8,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Jumper extends Tile
 {
+    //Store the character instance here
+    GreenCharacter greenCharacter;
+    
     public Jumper()
     {
         super(new GreenfootImage("jumper0.png"));
@@ -16,5 +19,20 @@ public class Jumper extends Tile
     public void act()
     {
         // Add your action code here.
+    /**
+     * Check if the character is above the jumper.
+     */
+    public boolean aboveJumper()
+    {
+        //Is valid if the character is at 20 units left or right about the jumper.
+        for(int i=-20; i<=20; i++)
+        {
+            //Check the x & y value
+            if(((this.getX()+i)==greenCharacter.getX()) && (this.getY()>greenCharacter.getY()))
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
